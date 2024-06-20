b8ded23bc807f1dc343bbfc5fa0591a27dbd4c6c
Souvik De <souvik.de@sato-global.com>
2024-06-20 15:56:24 +0900

    add complete Navbar to the portfolio website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
 import logo from "../assets/souvik.png"
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-import { FaSquareXTwitter } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-  <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
-    <div className="flex-flex-shrink-0 items-center">
-        <img src={logo} alt=""></img>
+  <nav className="mb-20 flex items-center justify-between py-6">
+    <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-10" src={logo} alt=""></img>
     </div>
-    <div className="m-8 flex items center justify-center gap-4 text-2xl">
+    <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <FaLinkedin />
         <FaGithub />
         <FaSquareXTwitter/>
