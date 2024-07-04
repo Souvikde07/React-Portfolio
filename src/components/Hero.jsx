d883b2521e1d9556e72cb761015ce66211323c64
Souvik De <souvik.de@sato-global.com>
2024-07-05 01:33:38 +0900

    add changes to support CV downloading from the Navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
                     initial="hidden"
                     animate="visible"
                     className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text
-                    text-3xl tracking-tight text-transparent">
+                    text-3xl tracking-tight text-transparent text-center lg:text-left">
                         Backend Firmware Developer
                     </motion.span>
                     <motion.p
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,23 @@
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
-import { FaInstagram } from "react-icons/fa";
+import { TbBrandLeetcode } from "react-icons/tb";
 
 const Navbar = () => {
   return (
   <nav className="mb-20 flex items-center justify-between py-6">
     <div className="flex flex-shrink-0 items-center">
-        <span className="mx-2 w-10 text-3xl font-medium decoration-white tracking-wider">Sd</span>
+      <a onClick={()=>window.location.reload()}
+        className="mx-2 w-10 text-3xl cursor-pointer font-medium decoration-white tracking-wider">
+        Sd
+      </a>
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaSquareXTwitter/>
-        <FaInstagram />
+        <a href="https://www.linkedin.com/in/souvik-de-06a006137/" target="_blank"><FaLinkedin /></a>
+        <a href="https://github.com/Souvikde07" target="_blank"><FaGithub /></a>
+        <a href="https://x.com/dsouvik_07" target="_blank"><FaSquareXTwitter/></a>
+        <a href="https://leetcode.com/u/Souvikd07/" target="_blank"><TbBrandLeetcode /></a>
+        <a download="/SOUVIK DE RESUME_Jan24.pdf">CV</a>
     </div>
   </nav>
   );
