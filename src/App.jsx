673342f18a8a1d81317176800e88b1f26405220b
Souvik De <souvik.de@sato-global.com>
2024-07-04 20:31:43 +0900

    add Experience section to the website

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
+import Experience from "./components/Experience";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Hero/>
         <About/>
         <Technologies/>
+        <Experience/>
       </div>
     </div>
   )
