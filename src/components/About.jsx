bed803a4307be610430264c4f874456282d615da
Souvik De <souvik.de@sato-global.com>
2024-07-30 14:24:50 +0900

    add skill-visualization table instead of the picture in the about me area

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,4 @@
-import aboutImg from "../assets/about.jpg";
-import { ABOUT_TEXT } from "../constants";
+import { ABOUT_TEXT,SKILLS } from "../constants";
 import { motion } from "framer-motion";
 
 const About = () => {
@@ -10,13 +9,31 @@ const About = () => {
             <span className="text-neutral-500"> Me</span>
         </h2>
         <div className="flex flex-wrap">
+            {/*Skills Table*/}
             <motion.div
             whileInView={{opacity:1, x: 0}}
             initial={{opacity: 0, x: -100}}
             transition={{duration: 0.5}}
             className="w-full lg:w-1/2 lg:p-8">
                 <div className="flex items-center justify-center">
-                    <img className="rounded-2xl" src={aboutImg} alt="about" />
+                    <table className="min-w-full divide-y divide-neutral-400 border border-neutral-400">
+                        <thead className="bg-white border-b border-neutral-400">
+                            <tr>
+                                <th className="py-2 px-4 font-semibold text-neutral-900 border-r border-neutral-500">Skill Area</th>
+                                <th className="py-2 px-4 font-semibold text-neutral-900 border-r border-neutral-500">Technology</th>
+                                <th className="py-2 px-4 font-semibold text-neutral-900">Proficiency</th>
+                            </tr>
+                        </thead>
+                        <tbody className="bg-neutral-700 divide-y divide-neutral-500">
+                            {SKILLS.map((skill,index) => (
+                                <tr key={index}>
+                                    <td className="py-2 px-4 border-r border-neutral-500">{skill.skillArea}</td>
+                                    <td className="py-2 px-4 border-r border-neutral-500">{skill.technologies}</td>
+                                    <td className="py-2 px-4">{skill.proficiency}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
                 </div>
             </motion.div>
             <motion.div
@@ -25,7 +42,7 @@ const About = () => {
             transition={{duration: 0.5}}
             className="w-full lg:w-1/2">
                 <div className="flex justify-center lg:justify-start">
-                    <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
+                    <p className="my-6 max-w-xl py-6">{ABOUT_TEXT}</p>
                 </div>
             </motion.div>
         </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,9 +3,35 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable applications. With 4 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `Welcome to my portfolio! As a dedicated full stack developer, I specialize in designing and building efficient, scalable applications. Over the past four years, I have refined my expertise in both front-end frameworks like React and Next.js, and back-end technologies such as Node.js, MySQL, PostgreSQL, and MongoDB. My mission is to utilize my comprehensive skill set to develop cutting-edge solutions that propel business success and provide outstanding user experiences. Dive into my projects and discover how I can transform your vision into reality!`;
+export const SKILLS = [
+  {
+    skillArea: "Front-End Development",
+    technologies: "React",
+    proficiency: "★★★★★"
+  },
+  {
+    skillArea: "Back-End Development",
+    technologies: "Node.js (with Express.js), Python",
+    proficiency: "★★★★★, ★★★★☆"
+  },
+  {
+    skillArea: "Databases",
+    technologies: "MySQL, PostgreSQL, MongoDB",
+    proficiency: "★★★★★, ★★★★★, ★★★★☆"
+  },
+  {
+    skillArea: "Embedded Systems",
+    technologies: "C++",
+    proficiency: "★★★★★"
+  }
+]
+export const ABOUT_TEXT = `I am a versatile Embedded Systems Developer with a strong commitment to creating efficient and user-friendly applications. 
 
-export const ABOUT_TEXT = `I am a dedicated and versatile Embedded Systems Developer with a passion for creating efficient and user-friendly applications. With 4 years of professional experience in Software Development, I have worked with a variety of technologies, including C++(For optimazation of Embedded Printer Applications), React(For Web-Development), Python, Node.js)(With Express.js), MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, Competitive Coding and contributing to open-source projects.`;
+My journey into web development was fueled by a deep curiosity about how things work, evolving into a career where I constantly seek to enhance my skills and adapt to new challenges. I excel in collaborative environments, relishing the opportunity to tackle complex problems and deliver top-notch solutions.
+
+Beyond coding, I stay active, explore emerging technologies, engage in competitive coding, and contribute to open-source projects. Dive into my portfolio to see how my continuous learning and diverse experiences can bring innovative solutions to your projects.
+`;
 
 export const EXPERIENCES = [
   {
